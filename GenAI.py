1046ee686c5e71abd3c54ce78eb1fb95c76a2502
sactyagi-hub <sactyagi@gmail.com>
2026-06-26 18:58:41 +1000

    Update GenAI.py

--- GenAI.py
+++ GenAI.py
@@ -13,12 +13,18 @@ st.markdown("""
 today = datetime.date.today().strftime("%B %d, %Y")
 #robo = genai.Client(api_key="MY_API")
 robo= genai.Client(api_key=st.secrets["MY_API"])
-myChat=robo.chats.create(model="gemini-3.1-flash-lite")
+#myChat=robo.chats.create(model="gemini-3.1-flash-lite")
+myChat = robo.chats.create(
+    model="gemini-2.5-flash", # Note: gemini-2.5-flash is the standard recommended model
+    config=types.GenerateContentConfig(
+        system_instruction=f"You are Tyagi ji ka AI Assistant. Today's real-world date is {today}. Always use this date as your anchor for current events or relative time questions."
+    )
+)
 response_placeholder = st.empty()
 question = st.text_input("", placeholder= "Question : ")
 col1, col2, col3 = st.columns([4,1,4])
 with col2:
     send=st.button("Send")
-if send:
+if send and question:
     response=myChat.send_message(question)
     response_placeholder.write(response.text)
